d654a7c7e83ce6d4600dd2e73e6d1cee2f1875b7
Unknown <cbswatkins@gmail.com>
2017-06-05 12:45:39 -0600

    Inclass switch and ifelse statements
    this was done in visual studio due to unity licsense being expired.

---
+++ Inclass2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+public class Class1;
+{
+
+//private makes that data only read by this script
+    private int key = "gold"; //source
+
+//Function, script with runs a block of code.
+void story (key); //source check
+    switch(key)//source result
+    { 
+        case"gold":
+            print("Mary had a little lamb."); 
+        break;
+        case"silver":
+            print("Jack and the beanstalk."); 
+        break;
+        default:
+            print("out of luck, pal!");
+        break;
+    }
+
+void song(selection);
+    switch(selection)
+    {
+        case"gold":
+            print("Mary had a little lamb."); 
+        break;
+        case"silver":
+            print("Stairway to Heaven."); 
+        break;
+        default:
+            print("Wammy noise.");
+        break;
+     }   
+{
+	public Class1()
+	{
+        //if statement    if/else, if/elseif/else, or if/elseif/elseif/elseif/elseif/elseif/else.
+        string lightColor = "green";
+        
+
+        if (lightColor == "green")
+        {
+            print("Go the light is green");
+        }
+
+        else if (lightColor == "yellow")
+        {
+            print("The light is yellow, start to stop.");
+        }
+
+        else
+        {
+            print("The light is red, STOP!!!!");
+        }
+        
+        //switch statement  Case then break
+
+        public string jedi = "Luke";
+    
+        switch(jedi){
+        case "Ben":
+            print("These are not the droids you are looking for.");
+        break;
+        case "Luke":
+            print("I have a bad feeling.");
+        break;
+        case "ObiWan":
+            print("Use the force Luke.");
+        break;
+        case "Anakin":
+            print("May the force be with you.");
+        break;
+        default:
+            print("Judge me by my size, do you not!");
+        break; 
+
+}
+
+
+
+
+}
+
+
