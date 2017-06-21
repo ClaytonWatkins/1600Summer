281e8f12c3eea0b5848336676303277e30a53006
Unknown <cbswatkins@gmail.com>
2017-06-21 11:38:53 -0600

    Functions and methods
    Functions are an on demand machine that preforms a specific action.  It can be written to add coins or lumber to your resource pool.  It can be written to keep a closed until the player opens it by turning the knob.  They are repeatable but limited to the scope they was written for.

--- New Unity Project 1/Assets/ScopeAccess.cs
+++ New Unity Project 1/Assets/ScopeAccess.cs
@@ -3,5 +3,21 @@ using System.Collections;
 
 public class ScopeAccess : MonoBehaviour {
 
-public int coins = 0;
+	public int coins = 0;
+	public int num2;
+
+	void OnEnable(){
+		int num = AddCoin(coins, num2);
+		print(this.name + " has " + num + " Coins");
+	}
+
+	public int AddCoin(int coins, int num2){
+
+		int result = coins;
+        while(coins <100){
+            coins = coins + num2;
+        }
+
+		return coins;
+	}
 }
---
+++ New Unity Project 1/Assets/Toaster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toaster : MonoBehaviour {
+	 
+	/*int slices = 15
+
+	void Toaster(int slot1, int slot2)
+	{
+		if(slot1 == 1 || slot2 == 1){
+			print(this.name + " is toasted.");
+			
+		}
+		else if(slot1 == 1 && slot2 == 1){
+			print(this.name + " are toasted.");
+		}
+		else{
+			print("Toaster grows angery and demands to a sacrifice to the TOAST GAWDS")
+		}
+	}
+	Toaster(1,1); */
+
+}
+
