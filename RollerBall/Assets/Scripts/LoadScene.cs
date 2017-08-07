33fd6fdf89c5455c87f974854c1307ef99bf3a01
Unknown <cbswatkins@gmail.com>
2017-08-07 10:06:49 -0600

    menu screen

---
+++ RollerBall/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+
+	public void StartGame (){
+		SceneManager.LoadScene(1);
+	}
+}
