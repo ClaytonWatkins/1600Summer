fc5639cfeee46ec4b133bc00826be0498e2e464c
Unknown <cbswatkins@gmail.com>
2017-08-11 12:36:30 -0600

    ForLoops

---
+++ 1600 Summer Day 2/Assets/ForLoops.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoops : MonoBehaviour {
+
+int coins = 0;
+
+void Start(){
+	for (coins i = 0; i >= pickUpCoins; i+= pickUpCoins){
+		print ("Coins")
+	}
+}
+}
