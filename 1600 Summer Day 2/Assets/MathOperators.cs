d3476c73d71e6e6dfca26723f3d7d34be402a318
Unknown <cbswatkins@gmail.com>
2017-08-11 11:55:56 -0600

    Math ops

---
+++ 1600 Summer Day 2/Assets/MathOperators.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathOperators : MonoBehaviour {
+
+public int health;
+
+
+void multiply (){
+	if (3 * 3 = 9);
+	print ("9");
+}
+void division (){
+	if (9 / 3 = 3);
+	print ("3");
+}
+void addition (){
+	if (2 + 2 = 4);
+	print ("4");
+}
+void subtraction (){
+	if ( 4 - 2 = 2);
+	print ("2");
+}
+void multipleOperators (){
+	if (3 * 3, 2 + 2 = 13);
+	print ("unlucky you")
+}
+void usingExistingData (){
+	if (health != null);
+	health -= 2;
+	print ("health" + "-2 damage.")
+}
+void usingExistingDataFromOtherScripts (){
+	other.GetComponent<EnemyHealth>;
+	if (EnemyHealth >= 0); 
+	EnemyHealth -= 5;
+	print ("EnemyHealth" + "-5 damage");
+}
+void simpleMath (){
+	while (health <= 100){
+		yield return new WaitForSeconds(3);
+			health += 3;
+			print ("+3 to health");
+			}
+		}
+void evenSimplier (){
+	health ++;
+	print ("+1 to health")
+}
+}
+
+}
+} 
+}
+}
+}
+}
