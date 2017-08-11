3a2e3158c15f1b47da967e4703577749ea10f7c2
Unknown <cbswatkins@gmail.com>
2017-08-11 12:22:44 -0600

    swithstatment/whileloops

---
+++ 1600 Summer Day 2/Assets/Swithstatments.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swithstatments : MonoBehaviour {
+
+public health;
+
+void displayHealthBarColor(){
+	while (health){
+		case >100%:
+			set bar color to green;
+			break;
+		case >75%:
+			set bar color to blue;
+			break;
+		case >50%:
+			set bar color to orange;
+			break;
+		case >25%:
+			set bar color to red and flash;
+			break;
+		case <0%:
+			set bar color to black and start crumble animation;
+			break;
+	}
+}
+}
